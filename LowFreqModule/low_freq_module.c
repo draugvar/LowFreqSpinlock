40be89cd97286310b3527fc624e3c8c7cd5123bc
draugvar <draugvar@gmail.com>
2016-10-25 12:17:57 +0200

    creating low freq module

---
+++ LowFreqModule/low_freq_module.c
@@ -0,0 +1,5 @@
+//
+// Created by Salvatore Rivieccio on 25/10/16.
+//
+
+#include "low_freq_module.h"
---
+++ LowFreqModule/low_freq_module.h
@@ -0,0 +1,8 @@
+//
+// Created by Salvatore Rivieccio on 25/10/16.
+//
+
+#ifndef CLIONPROJECTS_LOW_FREQ_MODULE_H
+#define CLIONPROJECTS_LOW_FREQ_MODULE_H
+
+#endif //CLIONPROJECTS_LOW_FREQ_MODULE_H
